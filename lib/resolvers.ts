9935e5dce7ff60fd6a9130b38a5b0a655bcb063a
EmilienKopp <emilien.kopp@gmail.com>
2023-12-24 02:51:01 +0900

    feat: operator resolution for gt,gte,lt,lte

--- lib/resolvers.ts
+++ lib/resolvers.ts
@@ -2,6 +2,10 @@
 const equalOperators = ['=', '==', '===', 'eq', 'equals', 'equal'];
 const includeOperators = ['in', 'include', 'includes', 'within','contain','contains'];
 const notEqualOperators = ['not', '!=','!==', '<>','is not', 'not eq', '! eq'];
+const greaterThanOperators = ['>', 'gt', 'greater than'];
+const lesserThanOperators = ['<', 'lt', 'less than'];
+const gteOperators = ['>=', 'gte', 'greater than or equal to'];
+const lteOperators = ['<=', 'lte', 'less than or equal to'];
 
 type ResolvedOperator = {
     operator: string,
@@ -17,6 +21,19 @@ export const resolveOperator = (operator: string): ResolvedOperator => {
 
     if(notEqualOperators.includes(operator))
         return { operator: 'eq', negated: true };
+
+    if(greaterThanOperators.includes(operator))
+        return { operator: 'gt', negated: false };
+
+    if(lesserThanOperators.includes(operator))
+        return { operator: 'lt', negated: false };
+
+    if(gteOperators.includes(operator))
+        return { operator: 'gte', negated: false };
+
+    if(lteOperators.includes(operator))
+        return { operator: 'lte', negated: false };
+
 
     return {operator, negated: false};
 }
