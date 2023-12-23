fb8cdb8bb422bb1eb0829020b01d40c309333655
EmilienKopp <emilien.kopp@gmail.com>
2023-12-24 02:49:58 +0900

    build: test coverage settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,20 @@ export default defineConfig({
           name: 'supakit-eloquent', // The global variable name in UMD builds
           fileName: (format) => `supakit-eloquent.${format}.js`
         },
-    }
+    },
+    test: {
+      coverage: {
+        enabled: true,
+        exclude: [
+          'tests/**/*.{ts,tsx}', 
+          'node_modules/**/*.{ts,tsx}', 
+          'dist/**/*.{ts,tsx}',
+          'src/**/*.{ts,tsx}',
+          'supakit-eloquent*',
+          'types/**/*.{ts,tsx}'
+        ],
+
+      },
+      include: ['tests/**/*.{ts,tsx}'],
+    },
 });
